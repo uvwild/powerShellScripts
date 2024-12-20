e9073d01f2f05308c5f158d6e3845cfd69095a9e
UV <uvwild@gmail.com>
2024-12-20 07:50:46 +0100

    starting to work

--- Scripts/StartSoundBath.ps1
+++ Scripts/StartSoundBath.ps1
@@ -1,25 +1,59 @@
-#StartSoundBath.ps1
+# StartSoundBath.ps1
+[CmdletBinding()]
 
-# get list of audio devices
+param (
+    [Alias("v")]
+    [switch]$ShowDetails
+)
+
+# Install the AudioDeviceCmdlets module if not already installed
 Install-Module -Name AudioDeviceCmdlets -Scope CurrentUser -Force
-$audioDevices = Get-AudioDevice -List
-# $audioDevices | ForEach-Object {
-#     Write-Host "Name: $($_.Name)"
-#     Write-Host "Type: $($_.Type)"
-#     Write-Host "Device ID: $($_.Id)"
-#     Write-Host "----------------------"
-# }
 
-# find the one with Studio 24c
+function Show-AudioDeviceProperties {
+    param (
+        [Parameter(Mandatory=$true)]
+        [System.Management.Automation.PSObject]$audioDevice
+    )
+
+    Write-Host "Device: $($audioDevice.Name)" -ForegroundColor Cyan
+    $audioDevice | Get-Member -MemberType Properties | ForEach-Object {
+        $propertyName = $_.Name
+        $propertyValue = $audioDevice.$propertyName
+        Write-Host -ForegroundColor Blue -NoNewline "${propertyName}: "
+        Write-Host -ForegroundColor Yellow "$propertyValue"
+    }
+    Write-Host "----------------------" -ForegroundColor Green
+}
+
+# Get list of audio devices
+$audioDevices = Get-AudioDevice -List | Where-Object { $_.Type -eq 'Playback' }
+Write-Host "Retrieved list of playback audio devices." -ForegroundColor Green
+
+# Show properties of all audio devices if -ShowDetails is provided
+if ($ShowDetails) {
+    $audioDevices | ForEach-Object {
+        Show-AudioDeviceProperties -audioDevice $_
+    }
+}
+
+# Find the device with Studio 24c
 $studio24cDevice = $audioDevices | Where-Object { $_.Name -like "*Studio 24c*" }
 
-# specify the audio file
-$audioFile = "C:\Users\uv\Music\Moods\174Hz _ Pain Relief Pure Tone Sleep Music _ DEEPEST Healing Solfeggio Frequency on the Planet.mp3"
+# Specify the audio file
+$audioFile = 'C:\\Users\\uv\\Music\\Moods\\174Hz _ Pain Relief Pure Tone Sleep Music _ DEEPEST Healing Solfeggio Frequency on the Planet.mp3'
 
-# use this information to set parameters to start vlc with a mp3 file
+# Log the audio file to be used
+Write-Host "Using audio file: $audioFile." -ForegroundColor Green
+
+# Use this information to set parameters to start VLC with the mp3 file
 if ($studio24cDevice) {
-    $deviceId = $studio24cDevice.Id
-    Start-Process "C:\Program Files\VideoLAN\VLC\vlc.exe" -ArgumentList "--aout=directsound --directx-audio-device=$deviceId", $audioFile
+    # Get the numeric index of the Studio 24c device
+    $deviceIndex = [int]$studio24cDevice.Index
+    Write-Host "[$(Get-Date)] Found Studio 24c device with index: $deviceIndex." -ForegroundColor Green
+
+    # Start VLC with the specified audio file
+    Start-Process "C:\\Program Files\\VideoLAN\\VLC\\vlc.exe" -ArgumentList "--aout=directsound", "--directx-audio-device=$deviceIndex", "`"$audioFile`""
+    Write-Host "Started VLC with the specified audio file." -ForegroundColor Green
 } else {
-    Write-Host "No audio device with 'Studio 24c' found."
+    Write-Host "No audio device with 'Studio 24c' found." -ForegroundColor Red
 }
